9f16d4f6f187ba2c69d10d10eba1aae2fa5b914d
sohamthakurdesai <35452409+sohamthakurdesai@users.noreply.github.com>
2018-01-16 12:04:32 +0530

    change in code.js

--- DOM/code.js
+++ DOM/code.js
@@ -1,4 +1,7 @@
 var access = document.getElementById("code9");
 var code = access.innerHTML;
 code = code + " midnight";
+access.innerHTML = "I sleep at";
 alert(code);
+code = access.innerHTML + " midnight";
+alert("Thus " + code);
