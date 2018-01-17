6ee3392e195eef9a526574e64dce9567a10817c8
sohamthakurdesai <35452409+sohamthakurdesai@users.noreply.github.com>
2018-01-17 14:56:46 +0530

    re-check-in

--- CompleteBattleShipGame/battleship.js
+++ CompleteBattleShipGame/battleship.js
@@ -32,19 +32,19 @@ var view = {
 var model = {
 
   boardSize: 7,
-  numShips: 1,
+  numShips: 3,
   shipLength: 3,
   shipsSunk: 0,
   ships: [{
     locations: ["06", "16", "26"],
     hits: ["", "", ""]
-  }/*, {
+  }, {
     locations: ["24", "34", "44"],
     hits: ["", "", ""]
   }, {
     locations: ["10", "11", "12"],
     hits: ["", "", ""]
-  }*/],
+  }],
 
   isSunk: function(ship) {
     for (var i = 0; i < this.shipLength; i++) {
